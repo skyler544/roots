da27ac1b8dd9b0f4023da33c4a810e91d021af16
Skyler Mayfield <skyler544@gmail.com>
2024-03-12 19:22:38 +0100

    make directories for use with no-littering

--- early-init.el
+++ early-init.el
@@ -1,1 +1,7 @@
 ;;; -*- lexical-binding: t -*-
+;;
+;; Basic initialization
+;; ----------------------------------------------------
+;; Create some directories for later use
+(unless (file-directory-p "var") (make-directory "var"))
+(unless (file-directory-p "etc") (make-directory "etc"))
