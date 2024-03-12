7095b14592ca4a30d04463560080b29963b4d7fd
Skyler Mayfield <skyler544@gmail.com>
2024-03-12 19:24:13 +0100

    when running on a mac, we want these settings

--- early-init.el
+++ early-init.el
@@ -14,3 +14,11 @@
 
 ;; Don't let `customize' output to `init.el'.
 (setq custom-file (expand-file-name "var/custom.el" user-emacs-directory))
+
+;; MacOS specific settings
+(when (string-equal system-type "darwin")
+  (add-to-list 'default-frame-alist '(undecorated-round . t))
+  (setq insert-directory-program "gls")
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'meta)
+  (setq manual-program "gman"))
