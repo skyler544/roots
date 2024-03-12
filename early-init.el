2844ad9472cf072843ad472a5a0920f5cc8a981d
Skyler Mayfield <skyler544@gmail.com>
2024-03-12 19:22:52 +0100

    if possible, redirect the eln cache so it lands in the var/ folder

--- early-init.el
+++ early-init.el
@@ -5,3 +5,9 @@
 ;; Create some directories for later use
 (unless (file-directory-p "var") (make-directory "var"))
 (unless (file-directory-p "etc") (make-directory "etc"))
+
+;; Redirect native-comp cache.
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name "var/eln-cache" user-emacs-directory))))
