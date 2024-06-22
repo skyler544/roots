39915ede64f721f3e76fb5faea37ad908051721b
Skyler Mayfield <skyler544@gmail.com>
2024-06-22 15:08:39 +0200

    merge branch 'overhaul'

--- early-init.el
+++ early-init.el
@@ -1,33 +1,28 @@
 ;;; -*- lexical-binding: t -*-
 ;;
-;; This is Roots. This config works with Emacs 29.1+; older versions may require
-;; manual tweaking. See the file `roots.org' for the documentation for this
-;; config. This file ensures that packages can be installed and then installs
-;; and configures `use-package' (declarative macro for configuring packages),
-;; `no-littering' (library for keeping `user-emacs-directory' organized),
-;; 'minions' (space saving package for the modeline), and `gcmh' (package for
-;; automating a common "fix" for excessive elisp garbage collection).
+;; This is Roots. This early-init file sets a few initial settings, enables the
+;; Emacs package manager, `use-package' (declarative macro for configuring
+;; packages), and some organizational packages for init-directory housekeeping.
 ;;
-;; `init.el' will not exist without tangling `roots.org'. Run make retangle in
-;; your shell to generate it.
 ;;
 ;; Basic initialization
 ;; ----------------------------------------------------
-;; Create some directories for later use
-(let ((var (expand-file-name (concat user-emacs-directory "var")))
-      (etc (expand-file-name (concat user-emacs-directory "etc"))))
-  (unless (file-directory-p var) (make-directory var))
-  (unless (file-directory-p etc) (make-directory etc)))
+(mapc (lambda (dir)
+        (let ((dir (concat user-emacs-directory dir)))
+          (unless (file-directory-p dir) (make-directory dir))))
+      '("var" "etc"))
 
-;; Redirect native-comp cache.
-(when (fboundp 'startup-redirect-eln-cache)
-  (startup-redirect-eln-cache
-   (expand-file-name "var/eln-cache" user-emacs-directory)))
+(startup-redirect-eln-cache
+ (concat user-emacs-directory "var/eln-cache"))
 
-;; Don't let `customize' output to `init.el'.
+(setq inhibit-startup-screen t)
+(setq inhibit-x-resources t)
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'fundamental-mode)
+(setq load-prefer-newer noninteractive)
+
 (setq custom-file (expand-file-name "var/custom.el" user-emacs-directory))
 
-;; MacOS specific settings
 (when (string-equal system-type "darwin")
   (add-to-list 'default-frame-alist '(undecorated-round . t))
   (setq insert-directory-program "gls")
@@ -38,40 +33,25 @@
 
 ;; Package management
 ;; ----------------------------------------------------
-;; Start up the built-in Emacs package manager.
 (package-initialize)
 
-;; Add a larger package repository.
-(unless (assoc-default "melpa" package-archives)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
 
-;; If the package list is empty, initialize it.
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Enable `use-package'; not necessary on Emacs 29.1+.
-;; `use-package' is used to declaratively configure packages.
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
 (use-package use-package
-  :config
-  ;; Install missing packages by default
-  (setq use-package-always-ensure t)
-
-  ;; Use lazy loading by default
-  (setq use-package-always-defer t)
-
-  ;; Be as informative as possible
-  (setq use-package-verbose t)
-  (setq use-package-compute-statistics t))
+  :custom
+  (use-package-always-ensure t)
+  (use-package-always-defer t))
 
 ;; Organization
 ;; ----------------------------------------------------
 (use-package no-littering :demand t)
 (use-package minions
   :hook (after-init . minions-mode)
-  :config (setq minions-mode-line-lighter "--"))
+  :custom (minions-mode-line-lighter "--"))
 
 ;; Garbage collection
 ;; ----------------------------------------------------
